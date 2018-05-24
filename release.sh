de2b0cabf2344a2e5ac251bd2e78e6b3b6722564
SoLi <353124127@qq.com>
2018-05-24 09:31:43 +0800

    上传脚本添加username key 打印

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@
 #rm -rf $HOME/.gradle/caches
 #rm -rf .gradle
 
+echo "BINTRAY_USERNAME：$BINTRAY_USERNAME"
+echo "BINTRAY_API_KEY：$BINTRAY_API_KEY"
+
 ./gradlew clean build bintrayUpload
 
 #./gradlew -P repositoryUrl=file://C://Users/Soli/.m2/repository uploadArchives
